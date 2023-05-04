98c8a5ccead533c509e1f04a4794be3e15e9cdb3
Carlos Almeida <ca86.pb@gmail.com>
2023-05-03 19:04:02 -0700

    Delete test directory

--- test/js/script.js
+++
@@ -1,8 +0,0 @@
-const anchors = document.querySelectorAll('.item a');
-
-anchors.forEach(anchor => {
-    anchor.addEventListener('click', event => {
-        event.preventDefault();
-        event.currentTarget.querySelector('img').classList.toggle('large');
-    });
-});
